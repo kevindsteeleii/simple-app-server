9967dfa5ce746e1843f9822b965d4bcc0b7ecd77
github username <github email address>
2019-01-18 04:34:52 -0500

    cors

--- app.js
+++ app.js
@@ -37,7 +37,14 @@ app.use((req, res, next) => {
   }
 })); */
 
+aapp.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/items', itemsRouter);
