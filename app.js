77eeb541a3a24fcc0e8d70ee33b6584da962bd57
github username <github email address>
2019-01-18 01:41:26 -0500

    express CORS fixes

--- app.js
+++ app.js
@@ -9,11 +9,28 @@ const itemsRouter = require('./routes/items');
 const testsRouter = require('./routes/tests');
 const app = express();
 
+const WhiteList = [ process.env.PROD_URL, 'http://localhost:3000/' ];
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (!origin) return callback(null, true);
+
+//     if(whitelist.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+
+//     return callback(null, true);
+//   }
+// }));
+
+app.use(cors({origin: process.env.PROD_URL || 'http://localhost:3000/'}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
